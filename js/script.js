120bdc79fe84762ee78f0e419d217bde2d9106f9
Kristhen <kristhensainz@hotmail.com>
2019-11-23 08:29:42 -0800

    Update

--- js/script.js
+++ js/script.js
@@ -5,12 +5,11 @@ const body = document.querySelector('body');
 let users = [];
 
 // Fetch request.
-
 fetch(url)
 .then(checkStatus)
 .then(res => res.json())
 .then(data => createGallery(data))
-.then(data => createModalWindow(data))
+.then(gallery => eventHandler(gallery))
 .catch(error => console.log('Looks like there was a problem. ', error));
 
 // To check if the request succeded.
@@ -23,8 +22,8 @@ function checkStatus(response) {
 }
 
 function createGallery(data){
-    users = data.results;
-    let randomUsers = users;
+    
+    let randomUsers = data.results;
 
     randomUsers.forEach(user => {
         const galleryHtml = `
@@ -41,54 +40,60 @@ function createGallery(data){
 
         gallery.innerHTML += galleryHtml;
     });
-    
-}
-
-const createModalDiv = document.createElement('div');
-    createModalDiv.className = "modal-container"
-    body.append(createModalDiv);
-    const modalDiv = document.querySelector('.modal-container');
-    modalDiv.style.display = 'none';
-function createModalWindow(data){
-    
-    let randomUsers = users;
-    
-    randomUsers.forEach( user => {
-
-        const birthDay = user.dob.date.substring(8, 10);
-        const birthMonth = user.dob.date.substring(5, 7);
-        const birthYear = user.dob.date.substring(0, 4);
-        const modalHtml = `
-            <div class="modal">
-                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                <div class="modal-info-container">
-                    <img class="modal-img" src="${user.picture.large}" alt="profile picture">
-                    <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
-                    <p class="modal-text">${user.email}</p>
-                    <p class="modal-text cap">${user.location.city}</p>
-                    <hr>
-                    <p class="modal-text">${user.cell}</p>
-                    <p class="modal-text">${user.location.street}, ${user.location.city}, ${user.location.state} ${user.location.postcode}</p>
-                    <p class="modal-text">Birthday: ${birthMonth}/${birthDay}/${birthYear}</p>
-                </div>
-            </div>    
-            
-        `;
-        modalDiv.innerHTML += modalHtml;
         
-              
-    });
+    return gallery;
+}
 
-    body.appendChild(modalDiv);
+function createModalWindow(data){
+   
+    const modalDiv = document.createElement('div');
+    modalDiv.className = "modal-container"
+    
+    
+    const birthDay = user.dob.date.substring(8, 10);
+    const birthMonth = user.dob.date.substring(5, 7);
+    const birthYear = user.dob.date.substring(0, 4);
+    const modalHtml = `
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+                <p class="modal-text">${user.email}</p>
+                <p class="modal-text cap">${user.location.city}</p>
+                <hr>
+                <p class="modal-text">${user.cell}</p>
+                <p class="modal-text">${user.location.street}, ${user.location.city}, ${user.location.state} ${user.location.postcode}</p>
+                <p class="modal-text">Birthday: ${birthMonth}/${birthDay}/${birthYear}</p>
+            </div>
+        </div>    
+                
+    `;
+    modalDiv.innerHTML += modalHtml;
 
+    gallery.after(modalDiv);
     
 }
 
-const card = document.querySelectorAll('.card');
-for (let i = 0 ; i < card.length; i++) {
-    card[i].addEventListener('click' , (e)=> {
-        modalDiv.style.display = '';
-        e.style.display = '';
-        createModalWindow();
-    });
+function eventHandler () {
+    console.log(gallery);
+    const cards = Array.from(document.querySelectorAll('.card'));
+    console.log(cards);
+
+    cards.forEach( card => {
+        card.addEventListener('click', (e) => {
+            createModalWindow(e.target);
+        })
+    })
+
+    // for (let i = 0 ; i < card.length; i++) {
+    //     card[i].addEventListener('click' , (e)=> {
+    //         createModalWindow(e);
+        
+    //         const btn = document.getElementById('modal-close-btn');
+    //         btn.addEventListener('click', () => {
+    //             modalDiv.style.display = 'none'
+    //         });
+    //     });
+    // }
 }
